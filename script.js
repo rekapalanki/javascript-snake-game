941285450aeb944155320d7ad060ce549e4b5a1b
Reka Palanki <reka.palanki@gmail.com>
2021-03-19 17:40:35 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Select elements on the page - canvas, shake button
+
+const canvas = document.querySelector('canvas');
+const clear = document.querySelector('.clear');
+
+// Setup canvas for drawing
+
+// Write draw function
+
+// Write handler for the keys
+
+// clear function
+
+// listen for arrow keys
